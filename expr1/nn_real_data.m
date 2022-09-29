535c17876ebc1f62de952d2cd037f4a7bb8fb5d7
matthewmerris <matthewmerris@u.boisestate.edu>
2022-09-29 12:24:16 -0600

    attempting to create non-neg test problems

--- expr1/nn_real_data.m
+++ expr1/nn_real_data.m
@@ -1,6 +1,37 @@
 % some basic params
 size = [100 100 100];
-R = 100;
+R = 5;
 num_runs = 10;
 
 losses = {'normal' 'rayleigh' 'gamma' 'huber (0.25)' 'beta (0.3)'};
+
+% containers for results
+times_opt = zeros(1, num_runs);
+times_gcp = zeros(num_losses, num_runs);
+
+fits_opt = zeros(1, num_runs);
+fits_gcp = zeros(num_losses, num_runs);
+
+scores_opt = zeros(1, num_runs);
+scores_gcp = zeros(num_losses, num_runs);
+
+% perform a series of runs on a naively generated artificial tensor
+
+for run = 1:num_runs
+    % create a problem
+    info = create_problem('Size', size, 'Num_factors', R, 'Factor_Generator', 'rand');
+    X = info.Data;
+    M_true = info.Soln;
+    % create a guess
+    M_init = create_guess('Data', X, 'Num_factors', R,'Factor_Generator', 'nvecs');
+    M_init = ktensor(M_init);
+    % feed problem and guess to gcp, cp-opt
+    for i = 1:num_losses
+        % decompose and compare with gcp
+        tic, [M_gcp, M_0, out_gcp] = gcp_opt(X, R, 'type', losses{i},'printitn', 0, 'init', M_init); 
+        times_gcp(i,run) = toc;
+        scores_gcp(i,run) = score(M_gcp, M_true, 'greedy', false);
+        fits_gcp(i,run) = 1 - norm(X-full(M_gcp))/norm(X);
+        fprintf('Final fit: %e \n\n', fits_gcp(i,run));
+    end
+end
