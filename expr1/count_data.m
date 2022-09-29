5b4dba2e5a3f748a729ccdaf8f1839c06d4ac910
matthewmerris <matthewmerris@u.boisestate.edu>
2022-09-29 11:39:27 -0600

    more scaffolding for experiment

--- expr1/count_data.m
+++ expr1/count_data.m
@@ -4,3 +4,12 @@ R = 100;
 num_runs = 10;
 
 losses = {'normal' 'poisson' 'poisson-log' 'negative-binomial (4)'};
+
+% perform a series of runs on a naively generated artificial tensor
+
+for 
+
+info = create_problem('Size', size, 'Num_factors', R);
+X = info.Data;
+M_true = info.Soln;
+
--- expr1/normal_data.m
+++ expr1/normal_data.m
@@ -4,3 +4,21 @@ R = 100;
 num_runs = 10;
 
 losses = {'normal' 'rayleigh'};
+num_losses = length(losses);
+
+% perform a series of runs on a naively generated artificial tensor
+
+for run = 1:num_runs
+    % create a problem
+    info = create_problem('Size', size, 'Num_factors', R);
+    X = info.Data;
+    M_true = info.Soln;
+    % create a guess
+    init_factors = create_guess('Data', X, 'Factor_Generator', 'nvecs');
+    % feed problem and guess to gcp, cp-opt
+    for i = 1:num_losses
+        % decompose and compare with gcp
+    end
+end
+
+
