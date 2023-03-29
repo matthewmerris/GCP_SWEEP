6b2dcf95012674177940dda4cd2a0100d414086a
matthewmerris <matthewmerris@u.boisestate.edu>
2023-03-29 13:13:52 -0600

    random initialization for naive baseline

--- expr_baseline/nn_baseline_naive.m
+++ expr_baseline/nn_baseline_naive.m
@@ -1,7 +1,8 @@
-%% Clear the workspace
+%% Clear the workspace & set up random seed
 clear; clc;
+rng('default');
 
-% check for results directory, make it if not there
+%% check for results directory, make it if not there
 datafolder = "results";
 if ~isfolder(datafolder)
     mkdir(datafolder)
@@ -9,7 +10,8 @@ end
 
 % general experiment paramenters 
 sz = [20, 20, 30];  % tensor size
-num_runs = 100;        % number of runs, i.e. number of tensors generated
+rank = 10
+num_runs = 1;        % number of runs, i.e. number of tensors generated
 ttypes = {'rand' 'randn' 'rayleigh' 'beta' 'gamma'}; % tensor generator types
 ltypes = {'normal' 'rayleigh' 'gamma' 'huber (0.25)' 'beta (0.3)'}; % GCP loss types
 
@@ -29,17 +31,26 @@ else
     fprintf("Output file exists ... not creating\n");
 end
 
-% Get into the experiment
+%% Get into the experiment
+
 num_types = length(ttypes);     % number of generators
 num_losses = length(ltypes);    % number of GCP loss functions
+num_modes = length(sz);         % number of tensor modes
 t_start = tic;
 for type = 1:num_types
     for run = 1:num_runs
         % generate data tensor
         ten = NN_tensor_generator_whole('Size', sz, 'Gen_type', ttypes{type});
         X = ten.Data;
+        %generate random initial guess
+        factors = cell(num_modes,1);
+        rng(12 + run);
+        for fct = 1:num_modes
+            factors{fct} = rand(sz(fct), rank)+.1;
+        end
         % perform decompositions with available loss functions
         for loss = 1:num_losses
+            
         end
     end
 end
