46b5d03907e05c8296b5a659377322085626cb51
matthewmerris <matthewmerris@u.boisestate.edu>
2023-03-30 16:02:03 -0600

    output to file implemented

--- expr_baseline/nn_baseline_naive.m
+++ expr_baseline/nn_baseline_naive.m
@@ -10,9 +10,9 @@ end
 
 % general experiment paramenters 
 sz = [20, 20, 30];  % tensor size
-rank = 10
+rank = 10;
 num_runs = 1;        % number of runs, i.e. number of tensors generated
-ttypes = {'rand' 'randn' 'rayleigh' 'beta' 'gamma'}; % tensor generator types
+ttypes = {'rand' 'randn' 'beta' 'gamma'}; % tensor generator types | 'rayleigh'
 ltypes = {'normal' 'rayleigh' 'gamma' 'huber (0.25)' 'beta (0.3)'}; % GCP loss types
 
 % prep a general output filepath seed, use datetime for uniqueness
@@ -25,7 +25,8 @@ outputfile = datafolder + "/" + "nn_base_naive-size_" + strcat(num2str(sz)) + ..
 if ~isfile(outputfile)
     fprintf("Output file DNE ... creating\n");
     fileID = fopen(outputfile,"w");
-    fprintf(fileID, "This will be a header row, most likely!\n");
+    % HEADER: Generator, Run, Loss, Time, Fit, Log-Like, Cos Sim
+    fprintf(fileID, "Generator,Run,Loss,Time,Fit,Cos Sim\n"); % Log-Like, 
     fclose(fileID);
 else
     fprintf("Output file exists ... not creating\n");
@@ -36,21 +37,33 @@ end
 num_types = length(ttypes);     % number of generators
 num_losses = length(ltypes);    % number of GCP loss functions
 num_modes = length(sz);         % number of tensor modes
+rng(13);
 t_start = tic;
 for type = 1:num_types
     for run = 1:num_runs
         % generate data tensor
         ten = NN_tensor_generator_whole('Size', sz, 'Gen_type', ttypes{type});
         X = ten.Data;
-        %generate random initial guess
+        % generate random initial guess
         factors = cell(num_modes,1);
-        rng(12 + run);
+        % rng(12 + run);
         for fct = 1:num_modes
             factors{fct} = rand(sz(fct), rank)+.1;
         end
+        M_init = ktensor(factors);
         % perform decompositions with available loss functions
         for loss = 1:num_losses
-            
+            tic, [M_gcp, M_0, out_gcp] = gcp_opt(X, rank, 'type',ltypes{loss}, 'printitn',0, 'init', M_init);
+            t = toc;
+            fit = 1 - norm(X-full(M_gcp))/norm(X);
+            % llike = tt_loglikelihood(X,M_gcp);
+            cossim = cosSim(X, M_gcp, num_modes);
+            % write results to file
+            fileID = fopen(outputfile, "a");
+            fprintf(fileID,"%s, %u, %s, %f, %f, %f, %f", ttypes{type}, run, ltypes{loss}, ...
+                                                            t, fit, cossim);
+            fprintf(fileID, '\n');
+            fclose(fileID);
         end
     end
 end
