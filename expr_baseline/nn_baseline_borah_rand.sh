e50ecae83c37f3691dc81257f7c6036c280bd47c
Matthew Merris <matthewmerris@u.boisestate.edu>
2023-04-27 01:24:59 -0600

    randn initialization

---
+++ expr_baseline/nn_baseline_borah_rand.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+### Specify job arguements
+
+#SBATCH -p bsudfq
+#SBATCH -N 1
+#SBATCH -n 48
+#SBATCH -t 01-12:00:00
+#SBATCH -o outfiles/%x-%j.out
+#SBATCH -J naive_nn
+
+
+### Get Matlab in loaded
+
+module load borah-applications
+module load matlab/r2020a
+
+
+### Get dipendencies added to Matlab path
+
+# MATLAB_TOOLS_PATH = "/home/mmerris/wares/matlab_tools"
+# EXPR_SRC_PATH = "/home/mmerris/GCP_SWEEPS"
+# matlab -nodisplay -nosplash -r "try, addlibrarypath(MATLAB_TOOLS_PATH, EXPR_SRC_PATH), catch me, fprintf('%s / %s\n',me.identifier,me.message), exit(1), end, exit(0)"
+
+cd $PWD
+CURRENTDATE=$( date +'%m-%d-%y-%H:%M:%S')
+OUTFILE="/bsuhome/mmerris/scratch/out_nn_${CURRENTDATE}.txt"
+matlab -nodisplay -nosplash -r  nn_baseline_naive_borah_rand_init >${OUTFILE}
---
+++ expr_baseline/nn_baseline_naive_borah_rand_init.m
@@ -0,0 +1,87 @@
+%% Clear the workspace & set up random seed
+clear; clc;
+rng('default');
+
+%% RUN THIS SECTION ON R2/BORAH ONLY!!! adds dependencies to path
+addpath(genpath("/bsuhome/mmerris/wares/matlab_tools"));
+addpath(genpath("/bsuhome/mmerris/GCP_SWEEP"));
+
+%% check for results directory, make it if not there
+datafolder = "/bsuhome/mmerris/scratch/GCPSWEEPS_nn_results";
+if ~isfolder(datafolder)
+    mkdir(datafolder)
+end
+
+% general experiment paramenters 
+sz = [100, 100, 100];  % tensor size
+rank = 10;
+num_runs = 1000;        % number of runs, i.e. number of tensors generated
+ttypes = {'rand' 'randn' 'rayleigh' 'beta' 'gamma'}; % tensor generator types | 
+ltypes = {'normal' 'rayleigh' 'gamma' 'huber (0.25)' 'beta (0.3)'}; % GCP loss types
+
+% prep a general output filepath seed, use datetime for uniqueness
+formatOut = 'yyyy_mm_dd_HH_MM_SS_FFF';
+dt = string(datetime("now"));
+
+outputfile = datafolder + "/" + "nn_base_naive-size_" + strcat(num2str(sz)) + ...
+            "-runs_" + num2str(num_runs) + "-" + dt + ".csv";
+
+if ~isfile(outputfile)
+    fprintf("Output file DNE ... creating\n");
+    fileID = fopen(outputfile,"w");
+    % HEADER: Generator, Run, Loss, Time, Fit, Cos-Sim, Est-Rank,
+    % Iterations
+    fprintf(fileID, "Generator,Run,Loss,Time,Fit,Cos Sim,Est Rank,Iters\n"); % Log-Like, 
+    fclose(fileID);
+else
+    fprintf("Output file exists ... not creating\n");
+end
+
+%% Get into the experiment
+
+num_types = length(ttypes);     % number of generators
+num_losses = length(ltypes);    % number of GCP loss functions
+num_modes = length(sz);         % number of tensor modes
+rng(13);
+parpool(48);  % Set pool size for parfor (8 nodes, 48 cores/node = 384 cores
+t_start = tic;
+for type = 1:num_types
+    % CONVERT FOLLOWING FOR-LOOP TO PARFOR-loop
+    for run = 1:num_runs
+        % generate data tensor
+        ten = NN_tensor_generator_whole('Size', sz, 'Gen_type', ttypes{type});
+        X = ten.Data;
+        % Estimate rank and initialize guess (based on modal unfolding svd)
+        [est_rank, factors] = estRank(X);
+%         % generate random initial guess
+%         factors = cell(num_modes,1);https://docs.google.com/presentation/d/1viwB11JV3gCcNCT8N3Tufr_UKEpmTu_hCGDiRN7EJLk/edit?usp=sharing
+%         % rng(12 + run);
+%         for fct = 1:num_modes
+%             factors{fct} = rand(sz(fct), rank)+.1;
+%         end
+        M_init = ktensor(factors);
+        % perform decompositions with available loss functions
+        for loss = 1:num_losses
+            tic, [M_gcp, M_0, out_gcp] = gcp_opt(X, est_rank, 'type',ltypes{loss}, 'printitn',0, 'init', factors);
+            t = toc;
+            fit = 1 - norm(X-full(M_gcp))/norm(X);
+            % llike = tt_loglikelihood(X,M_gcp);
+            cossim = cosSim(X, M_gcp, num_modes);
+            % write results to file
+            fileID = fopen(outputfile, "a");
+            fprintf(fileID,"%s,%u,%s,%f,%f,%f,%f,%f,%f", ttypes{type}, run, ltypes{loss}, ...
+                                                            t, fit, cossim, est_rank,out_gcp.lbfgsout.totalIterations);
+            fprintf(fileID, '\n');
+            fclose(fileID);
+        end
+    end
+end
+
+% Summerize the job size and time required
+
+t_total = toc(t_start);
+fprintf('%d runs for each of %d generators. | %d total generated tensors.\n', num_runs, num_types, num_runs*num_types);
+fprintf('GCP decomps using %d loss functions took %f minutes. | %d total decompositions performed. \n',num_losses, t_total/60, num_runs*num_types*num_losses);
+
+exit;
+
