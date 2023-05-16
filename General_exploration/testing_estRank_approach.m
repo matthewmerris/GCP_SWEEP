6c9b27067796020ba65456e36ef4e5fbed03232b
matthewmerris <matthewmerris@u.boisestate.edu>
2023-05-16 16:51:38 -0600

    avg rank experiment

--- General_exploration/testing_estRank_approach.m
+++ General_exploration/testing_estRank_approach.m
@@ -3,6 +3,47 @@
 % for estimating tensor rank and providing an initialization for the 
 % factor matrices of a CP decomposition of a data tensor.
 clear;
-size = [30, 40, 55];
-X = NN_tensor_generator_whole('Size', size);
-[rank, factors] = estRank(X.Data);
+sz = [5,5,5];
+ttypes = {'rand' 'randn' 'rayleigh' 'beta' 'gamma'}; % tensor generator types |
+num_types = length(ttypes);
+runs = 10;
+
+% Initialize results containers
+rand_ranks = zeros(runs,1);
+randn_ranks = zeros(runs,1);
+rayleigh_ranks = zeros(runs,1);
+beta_ranks = zeros(runs,1);
+gamma_ranks = zeros(runs,1);
+
+% Get to crunching
+parpool(48);
+for type = 1:num_types
+    parfor run = 1:runs
+        X = NN_tensor_generator_whole('Size', sz, 'Gen_type', ttypes{type});
+        switch ttypes{type}
+            case 'rand'
+                rand_ranks(run) = rankest(double(X.Data));
+            case 'randn'
+                randn_ranks(run) = rankest(double(X.Data));
+            case 'rayleigh'
+                rayleigh_ranks(run) = rankest(double(X.Data));
+            case 'beta'
+                beta_ranks(run) = rankest(double(X.Data));
+            case 'gamma'
+                gamma_ranks(run) = rankest(double(X.Data));
+        end
+    end
+end
+
+%% collect results and print
+avg_rand_rank = mean(rand_ranks);
+avg_randn_rank = mean(randn_ranks);
+avg_rayleigh_rank = mean(rayleigh_ranks);
+avg_beta_rank = mean(beta_ranks);
+avg_gamma_rank = mean(gamma_ranks);
+
+fprintf("avg rand rank: %f\n",avg_rand_rank);
+fprintf("avg randn rank: %f\n",avg_randn_rank);
+fprintf("avg rayleigh rank: %f\n",avg_rayleigh_rank);
+fprintf("avg beta rank: %f\n",avg_beta_rank);
+fprintf("avg gamma rank: %f\n",avg_gamma_rank);
