c8cc4522d6544a021dda56aad07edb532dcd6c86
matthewmerris <matthewmerris@u.boisestate.edu>
2023-09-25 11:12:57 -0600

    fiddling with normo rank estimation and initilization impacts

--- General_exploration/testing_normo.m
+++ General_exploration/testing_normo.m
@@ -3,18 +3,19 @@ rando = rng("default");
 F = 50;
 sz = [100 100 100];
 losses = {'normal' 'rayleigh' 'gamma' 'huber (0.25)' 'beta (0.3)'}; % GCP loss types
-gens = {'rand'}; %,'randn', 'rayleigh', 'beta', 'gamma'};
+gens = {'rand','randn', 'rayleigh', 'beta', 'gamma'};
 num_losses = length(losses);
 num_gens = length(gens);
-runs = 4;
+runs = 10;
 
 ranks = zeros(runs, num_gens);
 ranks_time = zeros(runs, num_gens);
-fits = zeros(runs, num_losses);
-corcondias = zeros(runs, num_losses);
+fits = zeros(num_gens, runs, num_losses);
+corcondias = zeros(num_gens, runs, num_losses);
 
-tic;
+start_time = tic;
 for i = 1:num_gens
+    disp("Tensor Generation Type: " + gens{i});
     for j = 1:runs
         % generate tensor
         X = NN_tensor_generator_whole("Size", sz, "Gen_type", gens{i});
@@ -23,18 +24,19 @@ for i = 1:num_gens
         [ranks(j,i), ranks_time(j,i)] = b_NORMO(data, F, 0.7, rando);
         % initialize solution
         Minit = cp_als(X.Data, ranks(j,i), 'maxiters', 2, 'printitn', 0);
+%         Minit = create_guess('Data', X.Data, 'Num_Factors', ranks(j,i), 'Factor_Generator', 'nvecs');
         for k = 1:num_losses
             % perform decomposition
             [M1,M0,out] = gcp_opt(X.Data, ranks(j,i), 'type', losses{k}, 'printitn', 0, 'init', Minit); 
-            % collect metrics
-            fits(j,k) = fitScore(X.Data, M1);
-            corcondias(j,k) = efficient_corcondia(X.Data, M1);
+            % collect metrics, index map: tensor gen X runs X loss type
+            fits(i,j,k) = fitScore(X.Data, M1);
+            corcondias(i,j,k) = efficient_corcondia(X.Data, M1);
         end
     end
 end
-total_time = toc;
+total_time = toc(start_time);
 disp("total time:\n");
-disp(total_time);
+disp(total_time / 60);
 
 %%
 
