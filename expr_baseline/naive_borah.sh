9d2722d5841053147be3511f75a2d1c37a9aa663
matthewmerris <matthewmerris@u.boisestate.edu>
2023-11-12 09:56:41 -0700

    fixed borah slurm script

--- expr_baseline/naive_borah.sh
+++ expr_baseline/naive_borah.sh
@@ -16,16 +16,7 @@ module load borah-applications
 module load matlab/r2020a
 
 
-### Get dipendencies added to Matlab path
-expr_directory="/bsuhome/mmerris/GCP_SWEEP"
-expr_dir_with_subs=$(find "$expr_directory" - type d | tr '\n' ':')
-export MATLABPATH=$expr_dir_with_subs$MATLABPATH
-
-tools_directory="/bsuhome/mmerris/wares/matlab_tools/active"
-tool_path_with_subs=$(find "$tools_directory" -type d | tr '\n' ':')
-export MATLABPATH=$tool_path_with_subs$MATLABPATH
-
 cd $PWD
 CURRENTDATE=$( date +'%m-%d-%y-%H:%M:%S')
 OUTFILE="/bsuhome/mmerris/scratch/out_nn_${CURRENTDATE}.txt"
-matlab -nodisplay -nosplash -r  nn_baseline_naive_borah_rand_init >${OUTFILE}  1 #!/bin/sh
+matlab -nodisplay -nosplash -r  nn_baseline_naive >${OUTFILE}  1 #!/bin/sh
