298b5cc47fac74fcfd3f03c6e7c1011f4ee59e68
matthewmerris <matthewmerris@u.boisestate.edu>
2024-01-29 19:39:13 -0700

    exploring corrected fitScore

--- helpers/fitScore.m
+++ helpers/fitScore.m
@@ -1,6 +1,7 @@
 function result = fitScore(X,M)
 %FITSCORE Summary of this function goes here
 %   Detailed explanation goes here
-result = abs(1 - normDiff(M,X)/norm(X));
+% result = abs(1 - normDiff(M,X)/norm(X));
+result = 1 - norm(X - full(M))/norm(X);
 end
 
