7963136fdef16c0d997f58ab9282e0a942d9dfa3
Matthew Merris <matthewmerris@u.boisestate.edu>
2024-09-02 13:09:30 -0600

    developing minimum krylov recursion using arnoldi-style iteration

--- General_exploration/min_kry_dev.m
+++ General_exploration/min_kry_dev.m
@@ -1,44 +1,89 @@
 %% generate a tensor
-sz = [20 25 30];
+sz0 = [20 25 30];
 nc = 5;
 
-prb = create_problem('Size', sz, 'Num_Factors', nc, ...
+prb = create_problem('Size', sz0, 'Num_Factors', nc, ...
     'Factor_Generator', 'stochastic');
 tns = prb.Data;
 
 %% gather relavent details for construction
-num_modes = ndims(tns);
-dimensions = size(tns);
-U = cell(num_modes,1);
-H = cell(num_modes,1);
+n = ndims(tns);
+sz = size(tns);
+U = cell(n,1);
+H = cell(n,1);
 k = nc;
 
-for idx = 1:num_modes
-    U{idx} = zeros(dimensions(idx),k);
-    H{idx} = zeros(k,k);
+for idx = 1:n
+    U{idx} = zeros(sz(idx),k + 1);
+    H{idx} = zeros(k+1,k);
     % populate 1st column of the first (num_modes - 1) modes
     % with rand
-    if idx ~= num_modes
-        u = rand(dimensions(idx),1);
-        U{idx}(:,1) = u;
+    if idx ~= n
+        u = rand(sz(idx),1);
+        % normalize and store
+        u = u / norm(u);
+    else
+        us = cell(1,n - 1);
+        modes = zeros(1,n - 1);
+        for jdx = 1:(n-1)
+        %     vecs.append(U{idx}(:,1));
+            us{1,jdx} = U{jdx}(:,1);
+            modes(1,jdx) = jdx;
+        end
+        u = ttv(tns,us, modes);
+        u = u / norm(u);
+    end
+    U{idx}(:,1) = u;
+end
+
+for kdx = 2:k           % kdx-th column
+    for jdx = 1:n       % jdx-th mode
+        us = cell(1, n - 1);
+        modes = zeros(1,n -1);
+        for i = 1:n
+            if i < jdx
+                us{1,i} = U{i}(:, kdx - 1);
+                modes(1,i) = i;
+            elseif i > jdx
+                us{1, i - 1} = U{i}(:, kdx - 1);
+                modes(1, i - 1) = i;
+            end
+        end
+        w = ttv(tns, us, modes);
+        for idx = 1:kdx % orthogonalization loop
+%             foo = U{jdx}(:,idx);
+%             bar = double(w);
+            H{jdx}(idx,kdx) = U{jdx}(:,idx)' * double(w);
+            w = double(w) - H{jdx}(idx,kdx) * U{jdx}(:,idx);
+        end
+        H{jdx}(kdx + 1, kdx) = norm(w);
+        if H{jdx}(kdx + 1, kdx) == 0
+            break;
+        end
+        % store in jdx-th mode basis
+        U{jdx}(:,kdx + 1) = w / H(kdx + 1, kdx);
     end
 end
 
 
 %%
 % tmp = tns;
-vecs = cell(num_modes - 1, 1);
-mode_mult = zeros(num_modes - 1, 1);
+us = cell(1,num_modes - 1);
+modes = zeros(1,num_modes - 1);
 for idx = 1:(num_modes-1)
-    vecs{idx} = squeeze(U{idx}(:,1));
-    mode_mult(idx) = idx;
+%     vecs.append(U{idx}(:,1));
+    us{1,idx} = U{idx}(:,1);
+    modes(1,idx) = idx;
 end
-tmp = ttv(tns,vecs, mode_mult);
-size(U{1}(:,1))
+
+tmp = ttv(tns,us, modes);
+% size(U{1}(:,1))
 
 %%
-vecs = {rand(sz(1),1), rand(sz(2),1)};
+vecs_1 = {rand(sz(1),1), rand(sz(2),1)};
 mds = [1 2];
 tmp = ttv(tns, vecs, mds);
 
 size(tmp)
+size(rand(sz(1),1))
+size(U{1}(:,1))
--- helpers/min_krylov_recursion.m
+++ helpers/min_krylov_recursion.m
@@ -1,12 +1,41 @@
-function [U,H] = min_krylov_recursion(A,u,v)
+function [U,H] = min_krylov_recursion(A, k)
 %MIN_KRYLOV_RECURSION Summary of this function goes here
 %   Detailed explanation goes here
-num_modes = ndims(A);
+n = ndims(A);
 sz = size(A);
-U = cell(num_modes,1);
-H = cell(num_modes,1);
+% initialize cell arrays for each mode's factor matrix and Hessian
+U = cell(n,1); 
+H = cell(n,1);
 
-% Initialize results containers
+% populate the first column of the first (n-1) factor matrices w/ 
+% uniform random vectors
+for idx = 1:n
+    U{idx} = zeros(sz(idx),k);
+    H{idx} = zeros(k+1,k);
+    % populate 1st column of the first (num_modes - 1) modes
+    % with rand
+    if idx ~= n
+        u = rand(sz(idx),1);
+        % normalize and store
+        u = u / norm(u);
+        U{idx}(:,1) = u;
+    end
+end
+
+% calculate first column of n-th mode factor matrix
+us = cell(1,n - 1);
+modes = zeros(1,num_modes - 1);
+for idx = 1:(n-1)
+%     vecs.append(U{idx}(:,1));
+    us{1,idx} = U{idx}(:,1);
+    modes(1,idx) = idx;
+end
+u = ttv(tns,us, modes);
+% normalize and store
+u = u / norm(u);
+U{n}(:,1) = u;
+
+% Ready to cycle
 
 
 outputArg1 = inputArg1;
