f804336bd81441d2da8134f45aba1eb307095bdd
matthewmerris <matthewmerris@u.boisestate.edu>
2024-10-09 16:13:51 -0600

    minor fixes to real world experiments

--- exprs_inits/init_exprs/expr6_realworld_sparse.m
+++ exprs_inits/init_exprs/expr6_realworld_sparse.m
@@ -1,23 +1,24 @@
 %% set-up data tensors
-amino_path = '~/datasets/real-world-rank-known/amino/claus.mat';
-dorrit_path = '~/datasets/real-world-rank-known/dorrit/dorrit.mat';
-eem_path = '~/datasets/real-world-rank-known/eem/EEM18.mat';
-sugar_path = '~/datasets/real-world-rank-known/sugar/sugar.mat';
+enron_path = '~/datasets/real-world-rank-unknown/enron/enron_emails.mat';
+lbnl_path = '~/datasets/FROSTT/lbnl_network/lbnl-network.tns';
+uber_path = '~/datasets/real-world-rank-unknown/tensor_data_uber/uber.mat';
+chi_path = '~/datasets/real-world-rank-unknown/tensor_data_chicago_crime/chicago_crime.mat';
+chi_2019_path = '~/datasets/real-world-rank-unknown/tensor_data_chicago_crime/chicago_crime_2019.mat';
 
-% amino_path = '/Users/matthewmerris/datasets/real-world-rank-known/amino/claus.mat';
-% dorrit_path = '/Users/matthewmerris/datasets/real-world-rank-known/dorrit/dorrit.mat';
-% eem_path = '/Users/matthewmerris/datasets/real-world-rank-known/eem/EEM18.mat';
-% sugar_path = '/Users/matthewmerris/datasets/real-world-rank-known/sugar/sugar.mat';
+% enron_path = '/Users/matthewmerris/datasets/real-world-rank-unknown/enron/enron_emails.mat';
+% uber_path = '/Users/matthewmerris/datasets/real-world-rank-unknown/tensor_data_uber/uber.mat';
+% chi_path = '/Users/matthewmerris/datasets/real-world-rank-unknown/tensor_data_chicago_crime/chicago_crime.mat';
+% chi_2019_path = '/Users/matthewmerris/datasets/real-world-rank-unknown/tensor_data_chicago_crime/chicago_crime_2019.mat';
 
-dataset_names = ["amino" "dorrit" "eem" "sugar"];
-ranks = [4 4 3 4];
-dataset_paths = {amino_path, dorrit_path, eem_path, sugar_path};
+dataset_names = ["enron" "uber" "chicago" "chicago_2019"];
+ranks = [10 21 10 10];
+dataset_paths = {enron_path, uber_path, chi_path, chi_2019_path};
 num_tensors = length(dataset_paths);
 
 num_runs = 10;
 tol = 1.0e-8;
 max_iters = 5000;
-inits = ["rand" "arnoldi" "min\_krylov" "nvecs" "gevd"];
+inits = ["rand" "arnoldi" "min\_krylov" "nvecs"];
 num_inits = length(inits);
 
 %% load and prep datasets
@@ -25,36 +26,22 @@ data_tns = cell(num_tensors,1);
 szs = cell(num_tensors,1);
 for kdx = 1:num_tensors
     load(dataset_paths{kdx});
-    if strcmp(dataset_names(kdx),"amino")
+    if strcmp(dataset_names(kdx),"enron")
         load(dataset_paths{kdx});
-        tns = tensor(X);
-        % % adjust to be non-negative
-        min_val = min(tns(:));
-        adj_by = -1 * min_val + 10*eps;
-        tns = tns + adj_by;
-    elseif strcmp(dataset_names(kdx),"dorrit")
+        tns = sptensor(Enron);
+    elseif strcmp(dataset_names(kdx),"uber")
         load(dataset_paths{kdx});
-        tns = tensor(EEM.data);
-        min_val = min(tns(:));
-        adj_by = -1 * min_val + 10*eps;
-        tns(isnan(tns(:)))=0;
-        tns = tns + adj_by;
-    elseif strcmp(dataset_names(kdx),"eem")
+        tns = sptensor(uber);
+    elseif strcmp(dataset_names(kdx),"chicago") || strcmp(dataset_names(kdx),"chicago_2019")
         load(dataset_paths{kdx});
-        tns = tensor(X);
-    elseif strcmp(dataset_names(kdx),"sugar")
-        load(sugar_path);
-        tns = tensor(X);
-        min_val = min(tns(:));
-        adj_by = -1 * min_val + 10*eps;
-        tns(isnan(tns(:)))=0;
-        tns = tns + adj_by;
-%         tns = reshape(tns,[268, 571,7]);
+        tns = sptensor(X);
+    elseif strcmp(dataset_names{kdx}, "lbnl")
+        tns = load_frostt(lbnl_path);
     else
         disp("Trouble now: dataset(s) requested DNE");
     end
     data_tns{kdx} = tns;
-    szs{kdx} = ndims(tns);
+    szs{kdx} = size(tns);
 end
 
 %% set-up initialization experiments
@@ -82,21 +69,8 @@ for jdx = 1:num_tensors
             t_nvecs = tic;
             init_nvecs = create_guess('Data',tns, 'Num_Factors', nc, 'Factor_Generator', 'nvecs');
             init_times(jdx, idx, 4) = toc(t_nvecs);
-            if ~strcmp(dataset_names(jdx),"uber") 
-                tns_matlab = full(tns);
-                t_gevd = tic;
-                [init_gevd,ot_gevd] = cpd_gevd(tns_matlab.data, nc);
-                init_times(jdx, idx, 5) = toc(t_gevd);
-                init_gevd = init_gevd';                     % trying to resolve gevd malfunction, likely source is negatives in the factors
-                % attempting to resolve by adjusting the factors to be
-                % non-negative
-                for i = 1:modes
-                    init_gevd{i} = init_gevd{i} - min(init_gevd{i},[],"all") + eps;
-                end
-            end
         else
             init_times(jdx, idx,4) = init_times(jdx, idx-1,4);
-            init_times(jdx, idx,5) = init_times(jdx, idx-1,5);
         end
         
         % ********************* perform decompositions
@@ -113,11 +87,6 @@ for jdx = 1:num_tensors
             [decomps_opt{jdx,idx,4,1},decomps_opt{jdx,idx,4,2},decomps_opt{jdx,idx,4,3}] ...  
                 = cp_opt(tns, nc, 'ftol', tol, 'gtol', tol, 'maxiters', max_iters, ...
                 'printitn', 0, 'lower', 0, 'init', init_nvecs);
-            if ~strcmp(dataset_names(jdx),"uber")
-                [decomps_opt{jdx,idx,5,1},decomps_opt{jdx,idx,5,2},decomps_opt{jdx,idx,5,3}] ... 
-                    = cp_opt(tns, nc, 'ftol', tol, 'gtol', tol, 'maxiters', max_iters, ...
-                    'printitn', 0, 'lower', 0, 'init', init_gevd);
-            end
         else
             [decomps_opt{jdx,idx,1,1},decomps_opt{jdx,idx,1,2},decomps_opt{jdx,idx,1,3}] ... 
                 = cp_opt(tns, nc, 'ftol', tol, 'gtol', tol, 'maxiters', max_iters, ...
@@ -133,7 +102,7 @@ for jdx = 1:num_tensors
 end
 
 % save results
-results_filename = sprintf('results/expr5_RW_%dtensor_%dinits_%druns_cp_opt_', num_tensors, num_inits, ...
+results_filename = sprintf('results/expr6_RW_%dtensor_%dinits_%druns_cp_opt_', num_tensors, num_inits, ...
     num_runs)+ string(datetime("now"));
 save(results_filename, 'szs', 'ranks','num_runs', 'tol', 'max_iters', 'num_tensors', 'num_inits', ...
-    'decomps_opt', 'init_times', 'inits', 'dataset_names');
+    'decomps_opt', 'init_times', 'inits', "dataset_names");
