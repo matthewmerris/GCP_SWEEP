1c8a1eb73207609bc5d1c7c2f11180b1a5a9ca39
matthewmerris <matthewmerris@u.boisestate.edu>
2024-12-02 16:36:00 -0700

    trying to resolve string scalar or character vector issue in new sweeps expr1

--- sweeps_exprs/expr1_baseline_structured.m
+++ sweeps_exprs/expr1_baseline_structured.m
@@ -1,21 +1,21 @@
 %% Generate data tensors of known rank
 sz = [100 100 100];
 rank = 10;
-gens = ['binary', 'rand', 'randn', ...
-    'orthogonal', 'stochastic'];
+gens = ["binary", "rand", "randn", ...
+    "orthogonal", "stochastic"];
 num_gens = length(gens);
 tens_per_gen = 10;
 data_tensors = cell(num_gens,tens_per_gen);
 
 
 for idx = 1:num_gens
-    if strcmp(gens(idx), 'binary')
+    if strcmp(gens{idx}, "binary")
         for jdx = 1:tens_per_gen
             [data_tensors{idx,jdx}, ~, ~] = create_problem_binary(sz,rank);
         end
     else
         for jdx = 1:tens_per_gen
-            [info, ~] = create_problem(sz,rank,'Factor_Generator', gens(idx));
+            [info, ~] = create_problem(sz,rank,'Factor_Generator', gens{idx});
             data_tensors{idx,jdx} =  info.Data;
         end
     end
