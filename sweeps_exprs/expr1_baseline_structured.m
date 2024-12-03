34c184a0f62f075eb851612cff646b57cac8c4b1
matthewmerris <matthewmerris@u.boisestate.edu>
2024-12-03 15:12:26 -0700

    CASE1 setup - Binary tensors

--- sweeps_exprs/expr1_baseline_structured.m
+++ sweeps_exprs/expr1_baseline_structured.m
@@ -15,10 +15,18 @@ for idx = 1:num_gens
         end
     else
         for jdx = 1:tens_per_gen
-            [info, ~] = create_problem(sz,rank,'Factor_Generator', gens{idx});
+            [info, ~] = create_problem('Size',sz,'Num_Factors',rank,'Factor_Generator', gens{idx});
             data_tensors{idx,jdx} =  info.Data;
         end
     end
 end
 
-% [tns, k, deets] = create_problem_binary(sz, rank);
+%% CASE1: Binary
+losses = ["normal", "huber (0.25)","binary", "bernoulli-logit"];
+num_losses = length(losses);
+mdl_Ms = cell(tens_per_gen, num_losses);
+mdl_infos = cell(tens_per_gen, num_losses);
+
+for idx = 1:tens_per_gen
+    for jdx = 1:num_losses
+        [mdl_Ms{idx,jdx}, ~, mdl_infos{idx,jdx}] = gcp_opt(data_tensors{1,idx}, )
