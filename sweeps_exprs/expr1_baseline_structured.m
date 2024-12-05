8605502a77697116561a5846bcbb2bd0bf0c9cdf
matthewmerris <matthewmerris@u.boisestate.edu>
2024-12-04 17:12:42 -0700

    metrics collection for CASE1

--- sweeps_exprs/expr1_baseline_structured.m
+++ sweeps_exprs/expr1_baseline_structured.m
@@ -21,17 +21,34 @@ for idx = 1:num_gens
     end
 end
 
-%% CASE1: Binary
+%% CASE1: Binary - Decompositions
 losses = ["normal", "huber (0.25)","binary", "bernoulli-logit"];
 num_losses = length(losses);
 mdl_Ms = cell(tens_per_gen, num_losses);
 mdl_infos = cell(tens_per_gen, num_losses);
 
+t_start = tic;
 for idx = 1:tens_per_gen
     sprintf("Tensor %d",idx)
     init = create_guess('Data', data_tensors{1,idx},'Num_Factors', rank);
     for jdx = 1:num_losses
         [mdl_Ms{idx,jdx}, ~, mdl_infos{idx,jdx}] = gcp_opt(data_tensors{1,idx}, rank, 'type', losses{1,jdx},...
             'printitn',0, 'init', init);
+    end
+end
+toc(t_start);
+
+%% CASE1: Binary - Collect metrics: FIT, COSSIM, COR, TIME/ITERS
+fits_case1 = zeros(tens_per_gen, num_losses);
+cossims_case1 = zeros(tens_per_gen, num_losses);
+cors_case1 = zeros(tens_per_gen, num_losses);
+times_case1 = zeros(tens_per_gen, num_losses);
+
+for idx = 1:tens_per_gen
+    for jdx = 1:num_losses
+        fits_case1(idx,jdx) = fitScore(data_tensors{1,idx}, mdl_Ms{idx,jdx});
+        cossims_case1(idx,jdx) = cosSim(data_tensors{1,idx}, mdl_Ms{idx,jdx}, 3);
+        [cors_case1(idx,jdx),~] = efficient_corcondia(data_tensors{1,idx}, mdl_Ms{idx,jdx});
+        times_case1(idx,jdx) = mdl_infos{idx,jdx}.mainTime;
     end
 end
