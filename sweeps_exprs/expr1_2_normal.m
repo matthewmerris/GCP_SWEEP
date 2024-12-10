24be4ae861ac89cc7dc024a986025e43b89b92da
matthewmerris <matthewmerris@u.boisestate.edu>
2024-12-10 13:35:52 -0700

    split expr1 - structured sweeps into 3 cases: binary, normal, nonneg

---
+++ sweeps_exprs/expr1_2_normal.m
@@ -0,0 +1,45 @@
+sz = [100 100 100];
+rank = 10;
+tens_per_gen = 100;
+data_tensors = cell(tens_per_gen,1);
+data_Ms = cell(tens_per_gen, 1);
+
+for jdx = 1:tens_per_gen
+    [data_tensors{jdx,1},data_Ms{jdx,1}, ~] = create_problem('Size',sz,'Num_Factors', rank, 'Factor_Generator', 'randn');
+end
+
+%% decompositions
+losses = ["normal", "huber (0.25)"];
+num_losses = length(losses);
+Ms_normal = cell(tens_per_gen, num_losses);
+infos_normal = cell(tens_per_gen, num_losses);
+
+t_start = tic;
+for idx = 1:tens_per_gen
+    sprintf("Tensor %d",idx)
+    init = arnoldi_cp_init(data_tensors{idx,1},rank);
+    for jdx = 1:num_losses
+        [Ms_normal{idx,jdx},~,infos_normal{idx,jdx}] = gcp_opt(data_tensors{idx,1}, rank, ...
+            'type', losses{1,jdx}, 'printitn',0, 'init',init);
+    end
+end
+toc(t_start);
+
+%% harvest metrics
+scores = zeros(tens_per_gen,num_losses);
+fits_full = zeros(tens_per_gen,num_losses);
+fits_Ms = zeros(tens_per_gen,num_losses);
+cossims = zeros(tens_per_gen,num_losses);
+cors = zeros(tens_per_gen,num_losses);
+times = zeros(tens_per_gen, num_losses);
+for idx = 1:tens_per_gen
+    for jdx = 1:num_losses
+        scores(idx,jdx) = score(Ms_normal{idx,jdx}, data_Ms{idx,1});
+        fits_full(idx,jdx) = fitScore(data_tensors{idx,1}, Ms_normal{idx,jdx});
+        fits_Ms(idx,jdx) = fitScore(full(data_Ms{idx,1}), Ms_normal{idx,jdx});
+        cossims(idx,jdx) = cosSim(data_tensors{idx,1}, Ms_normal{idx,jdx},3);
+        cors(idx,jdx) = efficient_corcondia(data_tensors{idx,1}, Ms_normal{idx,jdx});
+        times(idx,jdx) = infos_normal{idx,jdx}.mainTime;
+    end
+end
+
--- sweeps_exprs/expr1_baseline_structured.m
+++ sweeps_exprs/expr1_baseline_structured.m
@@ -1,7 +1,7 @@
 %% Generate data tensors of known rank
 sz = [100 100 100];
 rank = 10;
-gens = ["binary", "rand", "randn", ...
+gens = ["binary", "randn", "rand",...
     "orthogonal", "stochastic"];
 num_gens = length(gens);
 tens_per_gen = 10;
@@ -22,33 +22,98 @@ for idx = 1:num_gens
 end
 
 %% CASE1: Binary - Decompositions
-losses = ["normal", "huber (0.25)","binary", "bernoulli-logit"];
-num_losses = length(losses);
-mdl_Ms = cell(tens_per_gen, num_losses);
-mdl_infos = cell(tens_per_gen, num_losses);
+case1_losses = ["normal", "huber (0.25)","binary", "bernoulli-logit"];
+num_losses = length(case1_losses);
+case1_Ms = cell(tens_per_gen, num_losses);
+case1_infos = cell(tens_per_gen, num_losses);
 
 t_start = tic;
 for idx = 1:tens_per_gen
     sprintf("Tensor %d",idx)
     init = create_guess('Data', data_tensors{1,idx},'Num_Factors', rank);
     for jdx = 1:num_losses
-        [mdl_Ms{idx,jdx}, ~, mdl_infos{idx,jdx}] = gcp_opt(data_tensors{1,idx}, rank, 'type', losses{1,jdx},...
+        [case1_Ms{idx,jdx}, ~, case1_infos{idx,jdx}] = gcp_opt(data_tensors{1,idx}, rank, 'type', case1_losses{1,jdx},...
             'printitn',0, 'init', init);
     end
 end
 toc(t_start);
 
 %% CASE1: Binary - Collect metrics: FIT, COSSIM, COR, TIME/ITERS
-fits_case1 = zeros(tens_per_gen, num_losses);
-cossims_case1 = zeros(tens_per_gen, num_losses);
-cors_case1 = zeros(tens_per_gen, num_losses);
-times_case1 = zeros(tens_per_gen, num_losses);
+case1_fits = zeros(tens_per_gen, num_losses);
+case1_cossims = zeros(tens_per_gen, num_losses);
+case1_cors = zeros(tens_per_gen, num_losses);
+case1_times = zeros(tens_per_gen, num_losses);
 
 for idx = 1:tens_per_gen
     for jdx = 1:num_losses
-        fits_case1(idx,jdx) = fitScore(data_tensors{1,idx}, mdl_Ms{idx,jdx});
-        cossims_case1(idx,jdx) = cosSim(data_tensors{1,idx}, mdl_Ms{idx,jdx}, 3);
-        [cors_case1(idx,jdx),~] = efficient_corcondia(data_tensors{1,idx}, mdl_Ms{idx,jdx});
-        times_case1(idx,jdx) = mdl_infos{idx,jdx}.mainTime;
+        case1_fits(idx,jdx) = fitScore(data_tensors{1,idx}, case1_Ms{idx,jdx});
+        case1_cossims(idx,jdx) = cosSim(data_tensors{1,idx}, case1_Ms{idx,jdx}, 3);
+        [case1_cors(idx,jdx),~] = efficient_corcondia(data_tensors{1,idx}, case1_Ms{idx,jdx});
+        case1_times(idx,jdx) = case1_infos{idx,jdx}.mainTime;
     end
 end
+
+
+%% CASE2: Normal - Decompositions
+case2_losses = ["normal", "huber (0.05)"];
+num_losses = length(case2_losses);
+case2_Ms = cell(tens_per_gen, num_losses);
+case2_infos = cell(tens_per_gen, num_losses);
+
+t_start = tic;
+for idx = 1:tens_per_gen
+    sprintf("Tensor %d",idx)
+    init = create_guess('Data', data_tensors{2,idx},'Num_Factors', rank);
+    for jdx = 1:num_losses
+        [case2_Ms{idx,jdx}, ~, case2_infos{idx,jdx}] = gcp_opt(data_tensors{2,idx}, rank, 'type', case2_losses{1,jdx},...
+            'printitn',0, 'init', init);
+    end
+end
+toc(t_start);
+
+%% CASE2: Normal - Collect metrics: FIT, COSSIM, COR, TIME/ITERS
+case2_fits = zeros(tens_per_gen, num_losses);
+case2_cossims = zeros(tens_per_gen, num_losses);
+case2_cors = zeros(tens_per_gen, num_losses);
+case2_times = zeros(tens_per_gen, num_losses);
+
+for idx = 1:tens_per_gen
+    for jdx = 1:num_losses
+        case2_fits(idx,jdx) = fitScore(data_tensors{2,idx}, normalize(case2_Ms{idx,jdx}));
+        case2_cossims(idx,jdx) = cosSim(data_tensors{2,idx}, case2_Ms{idx,jdx}, 3);
+        [case2_cors(idx,jdx),~] = efficient_corcondia(data_tensors{2,idx}, case2_Ms{idx,jdx});
+        case2_times(idx,jdx) = case2_infos{idx,jdx}.mainTime;
+    end
+end
+
+%% CASE3: Non-negative (Uniform, Orthogonal, Stochastic) - Decompositions
+case3_losses = ["normal", "huber (0.25)","rayleigh", "gamma","beta (0.3)"];
+num_losses = length(case3_losses);
+case3_Ms = cell(3,tens_per_gen,num_losses);
+case3_infos = cell(3,tens_per_gen,num_losses);
+
+t_start = tic;
+for idx = 3:5
+    for jdx = 1:tens_per_gen
+        sprintf("Gen: %s, Ten: %d", gens{idx},jdx)
+        init = create_guess('Data', data_tensors{idx,jdx},'Num_Factors',rank);
+        tmp_i = idx - 2;
+        for kdx = 1:num_losses
+            [case3_Ms{tmp_i,jdx,kdx},~,case3_infos{tmp_i,jdx,kdx}] = gcp_opt(data_tensors{idx,jdx}, rank,...
+                'type', case3_losses{1,kdx}, 'printitn', 0, 'init', init);
+        end
+    end
+end
+toc(t_start);
+
+%% CASE3: Non-negative - Collect metrics: FIT, COSSIM, COR, TIME/ITERS
+case3_fits = zeros(3,tens_per_gen, num_losses);
+case3_cossims = zeros(3,tens_per_gen, num_losses);
+case3_cors = zeros(3,tens_per_gen, num_losses);
+case3_times = zeros(3,tens_per_gen, num_losses);
+
+for idx = 3:5
+    for jdx = 1:tens_per_gen
+        for kdx 1:num_losses
+            
+
